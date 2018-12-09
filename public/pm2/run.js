569b79545b31244aafad3c8cb45d0b9d702728f8
Frankie <101m06008@stud.sju.edu.tw>
2018-12-10 01:46:55 +0800

    [feat] add pm2 script

---
+++ public/pm2/run.js
@@ -0,0 +1,15 @@
+var spawn = require('child_process').spawn;
+
+/* 其实就是等于执行hexo server --draft*/
+free = spawn('php', ['artisan', 'schedule:run']);
+free.stdout.on('data', function (data) {
+    console.log('standard output:\n' + data);
+});
+
+free.stderr.on('data', function (data) {
+    console.log('standard error output:\n' + data);
+});
+
+free.on('exit', function (code, signal) {
+    console.log('child process eixt ,exit:' + code);
+});
